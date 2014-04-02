432f5cee8e9b6d9eec58678eb682a7b8a7a54f5d
Kiran Jonnalagadda <kiran@hasgeek.com>
2014-04-03 00:41:01 +0530

    Cookiefree now defers the workload to Baseframe

--- cookiefree/views/index.py
+++ cookiefree/views/index.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from flask import render_template, abort, redirect
-from flask.ext.assets import Bundle
-from baseframe import assets
-from coaster.utils import make_name
-from coaster.assets import split_namespec
+from flask import render_template, redirect, abort
+from baseframe.views import gen_assets_url
 from coaster.views import requestargs
+from coaster.assets import AssetNotFound
 from .. import app
 
 
@@ -18,21 +16,8 @@ def index():
 @requestargs('asset[]')
 def linkasset(asset):
     try:
-        names = [split_namespec(a)[0] for a in asset]
-    except ValueError:
-        abort(400)
-
-    is_js = reduce(lambda status, name: status and name.endswith('.js'), names, True)
-    is_css = reduce(lambda status, name: status and name.endswith('.css'), names, True)
-    output_name = make_name('-'.join(asset).replace(
-        '==', '-eq-').replace('>', '-gt-').replace('<', '-lt-').replace('>=', '-gte-').replace('<=', '-lte-'))
-    if is_js:
-        bundle = Bundle(assets.require(*asset), output='gen/' + output_name + '.js', filters='closure_js')
-    elif is_css:
-        bundle = Bundle(assets.require(*asset), output='gen/' + output_name + '.css', filters=['cssrewrite', 'cssmin'])
-    else:
-        abort(400)
-
-    response = redirect(bundle.urls(env=app.assets)[0])
+        response = redirect(gen_assets_url(asset))
+    except AssetNotFound:
+        abort(404)
     response.headers['Cache-Control'] = 'public, max-age=600'
     return response
