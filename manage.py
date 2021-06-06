5f0d4e94306bb9bcb0ec466c57cea53768bb1e6d
Kiran Jonnalagadda <kiran@hasgeek.com>
2021-06-07 03:08:48 +0530

    Upgrades for Flask 2.0

--- manage.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-from coaster.manage import init_manager
-from cookiefree import app
-
-
-if __name__ == '__main__':
-    manager = init_manager(app, None)
-    manager.run()
